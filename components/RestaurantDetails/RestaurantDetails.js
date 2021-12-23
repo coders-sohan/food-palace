106e3adad9ba5e76d5cf0c940d87742ed0c6a6a3
coders-sohan <coders.sohan.0216@gmail.com>
2021-12-23 22:17:14 +0600

    updated 1

---
+++ components/RestaurantDetails/RestaurantDetails.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function RestaurantDetails() {
+	return (
+		<View>
+			<Text>Restaurant Details</Text>
+		</View>
+	);
+}
